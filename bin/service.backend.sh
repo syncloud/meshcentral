8dc5f73efa33caae6c6626cb49e32351569dc13f
Boris Rybalkin <ribalkin@gmail.com>
2025-11-29 17:13:53 +0000

    fix start

--- bin/service.backend.sh
+++ bin/service.backend.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 /bin/rm -f $SNAP_COMMON/web.socket
-exec $SNAP/backend/bin/node $SNAP/opt/meshcentral/meshcentral/meshcentral --configfile $SNAP_DATA/config/config.json
+exec $SNAP/backend/bin/node $SNAP/opt/meshcentral/meshcentral/meshcentral.js --configfile $SNAP_DATA/config/config.json
