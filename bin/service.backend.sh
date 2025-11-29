dbbd1b20169a5a63f9cbe5da08ac501d7e859697
Boris Rybalkin <ribalkin@gmail.com>
2025-11-29 22:26:30 +0000

    path

--- bin/service.backend.sh
+++ bin/service.backend.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
 /bin/rm -f $SNAP_COMMON/web.socket
-exec $SNAP/backend/bin/node $SNAP/backend/opt/meshcentral/meshcentral/meshcentral.js --configfile $SNAP_DATA/config/config.json
+cd $SNAP/backend/opt/meshcentral/meshcentral
+exec $SNAP/backend/bin/node meshcentral.js --configfile $SNAP_DATA/config/config.json
