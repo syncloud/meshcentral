7393b31367542752598cf06d2433d558f1579a0a
Boris Rybalkin <ribalkin@gmail.com>
2025-11-30 10:56:02 +0000

    ls

--- backend/test.sh
+++ backend/test.sh
@@ -5,5 +5,5 @@ cd ${DIR}
 
 BUILD_DIR=${DIR}/../build/snap/backend
 ${BUILD_DIR}/bin/node --version
-ls -la.${BUILD_DIR}/opt/meshcentral/meshcentral/node_modules | grep node-forge
+ls -la ${BUILD_DIR}/opt/meshcentral/meshcentral/node_modules | grep node-forge
 ${BUILD_DIR}/bin/node ${BUILD_DIR}/opt/meshcentral/meshcentral/meshcentral.js --help
