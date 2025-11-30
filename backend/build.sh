964f853150f84f70b68d92ffef8635cd8245307d
Boris Rybalkin <ribalkin@gmail.com>
2025-11-30 21:47:51 +0000

    test libx

--- backend/build.sh
+++ backend/build.sh
@@ -12,6 +12,7 @@ npm install --no-audit --no-optional --omit=optional node-forge@1.3.2
 npm install --no-audit --no-optional --omit=optional passport@0.7.0
 npm install --no-audit --no-optional --omit=optional connect-flash@0.1.1
 npm install --no-audit --no-optional --omit=optional openid-client@5.7.1
+npm install --no-audit --no-optional --omit=optional ua-client-hints-js@0.1.2
 
 cp -r /etc ${BUILD_DIR}
 cp -r /opt ${BUILD_DIR}
@@ -21,7 +22,7 @@ cp -r /lib ${BUILD_DIR}
 cp -r ${DIR}/bin/* ${BUILD_DIR}/bin
 
 cd ${DIR}/../build
-wget https://github.com/cyberb/MeshCentral/archive/refs/heads/master.tar.gz
-tar xf master.tar.gz
-cp MeshCentral-master/meshcentral.js ${BUILD_DIR}/opt/meshcentral/meshcentral/
-cp MeshCentral-master/webserver.js ${BUILD_DIR}/opt/meshcentral/meshcentral/
+#wget https://github.com/cyberb/MeshCentral/archive/refs/heads/master.tar.gz
+#tar xf master.tar.gz
+wget https://raw.githubusercontent.com/cyberb/MeshCentral/refs/heads/master/meshcentral.js -O ${BUILD_DIR}/opt/meshcentral/meshcentral/meshcentral.js
+wget https://raw.githubusercontent.com/cyberb/MeshCentral/refs/heads/master/webserver.js -O ${BUILD_DIR}/opt/meshcentral/meshcentral/webserver.js
--- backend/test.sh
+++ backend/test.sh
@@ -5,5 +5,10 @@ cd ${DIR}
 
 BUILD_DIR=${DIR}/../build/snap/backend
 ${BUILD_DIR}/bin/node --version
-ls -la ${BUILD_DIR}/opt/meshcentral/meshcentral/node_modules | grep node-forge
+grep "1.3.2" ${BUILD_DIR}/opt/meshcentral/meshcentral/node_modules/node-forge/package.json
+grep "0.7.0" ${BUILD_DIR}/opt/meshcentral/meshcentral/node_modules/passport/package.json
+grep "0.1.1" ${BUILD_DIR}/opt/meshcentral/meshcentral/node_modules/connect-flash/package.json
+grep "5.7.1" ${BUILD_DIR}/opt/meshcentral/meshcentral/node_modules/openid-client/package.json
+grep "0.1.2" ${BUILD_DIR}/opt/meshcentral/meshcentral/node_modules/ua-client-hints-js/package.json
+
 ${BUILD_DIR}/bin/node ${BUILD_DIR}/opt/meshcentral/meshcentral/meshcentral.js --help
