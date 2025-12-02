5ab299d8987d257bda4c3d5456490e914949a68d
Boris Rybalkin <ribalkin@gmail.com>
2025-12-02 20:57:55 +0000

    try no --launch

--- bin/service.backend.sh
+++ bin/service.backend.sh
@@ -2,4 +2,4 @@
 /bin/rm -f $SNAP_COMMON/web.socket
 export NODE_ENV=production
 cd $SNAP_DATA
-exec $SNAP/backend/bin/node $SNAP/backend/opt/meshcentral/meshcentral/meshcentral.js --configfile $SNAP_DATA/config/config.json --launch
+exec $SNAP/backend/bin/node $SNAP/backend/opt/meshcentral/meshcentral/meshcentral.js --configfile $SNAP_DATA/config/config.json
