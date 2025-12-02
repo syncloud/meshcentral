51ca93b311467a777736263bdd2fb2116da8dbfc
Boris Rybalkin <ribalkin@gmail.com>
2025-12-02 21:21:32 +0000

    node path fix

--- bin/service.backend.sh
+++ bin/service.backend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 /bin/rm -f $SNAP_COMMON/web.socket
 export NODE_ENV=production
+export MESHCENTRAL_NODE_PATH=$SNAP/backend/bin/node
 cd $SNAP_DATA
-exec $SNAP/backend/bin/node $SNAP/backend/opt/meshcentral/meshcentral/meshcentral.js --configfile $SNAP_DATA/config/config.json
+exec $MESHCENTRAL_NODE_PATH $SNAP/backend/opt/meshcentral/meshcentral/meshcentral.js --configfile $SNAP_DATA/config/config.json
