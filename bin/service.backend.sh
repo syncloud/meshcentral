6ae8a6500b132e65f584f25b79b66126095d1ae4
Boris Rybalkin <ribalkin@gmail.com>
2025-12-03 20:23:30 +0000

    fix ca

--- bin/service.backend.sh
+++ bin/service.backend.sh
@@ -2,5 +2,6 @@
 /bin/rm -f $SNAP_COMMON/web.socket
 export NODE_ENV=production
 export MESHCENTRAL_NODE_PATH=$SNAP/backend/bin/node
+export NODE_EXTRA_CA_CERTS=/var/snap/platform/current/syncloud.ca.crt
 cd $SNAP_DATA
 exec $MESHCENTRAL_NODE_PATH $SNAP/backend/opt/meshcentral/meshcentral/meshcentral.js --configfile $SNAP_DATA/config/config.json
